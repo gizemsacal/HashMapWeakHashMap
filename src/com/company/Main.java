191ff59721894a1306e85fe8dfb31be6e41d5af2
gizem <gizemsacal@gmail.com>
2021-05-10 16:11:47 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.WeakHashMap;
+
+class Main {
+    public static void main(String[] args) {
+        // Creating a hashmap of even numbers
+        HashMap<String, Integer> evenNumbers = new HashMap<>();
+
+        String two = new String("Two");
+        Integer twoValue = 2;
+        evenNumbers.put(two, twoValue);
+        System.out.println("HashMap: " + evenNumbers);
+
+        // Creating a weak hash map from other hashmap
+        WeakHashMap<String, Integer> numbers = new WeakHashMap<>(evenNumbers);
+
+        System.out.println("WeakHashMap: " + numbers);
+    }
+}
